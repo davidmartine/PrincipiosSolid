1eea71d4baac55d4e0b423f6d17cf2441a9479bd
juand <juanda.mg@hotmail.com>
2023-11-19 13:29:11 -0500

    Aplicando Principio de abierto/cerrado

---
+++ 2-OpenClose/EmployeeContractor.cs
@@ -0,0 +1,18 @@
+namespace OpenClose
+{
+    public class EmployeeContractor : Employee
+    {
+        public EmployeeContractor(string fullname, int hoursWorked)
+        {
+            Fullname = fullname;
+            HoursWorked = hoursWorked;
+        }
+
+        public override decimal CalculateSalaryMonthly()
+        {
+            decimal hourValue = 10000M;
+            decimal salasy = hourValue * HoursWorked;
+            return salasy;
+        }
+    }
+}
--- 2-OpenClose/EmployeeFullTime.cs
+++ 2-OpenClose/EmployeeFullTime.cs
@@ -1,14 +1,21 @@
 namespace OpenClose
 {
-    public class EmployeeFullTime
+    public class EmployeeFullTime : Employee
     {
-        public string Fullname { get; set; }
-        public int HoursWorked { get; set; }
+        
 
         public EmployeeFullTime(string fullname, int hoursWorked)
         {
             Fullname = fullname;
             HoursWorked = hoursWorked;
-        }  
+        }
+
+        public override decimal CalculateSalaryMonthly()
+        {
+           decimal hourValue = 30000M;
+           decimal salary = hourValue * HoursWorked;
+           // Console.WriteLine($"Empleado: {employeeFullTime.Fullname}, Pago: {salary:C1} ");
+           return salary;
+        }
     }
 }
